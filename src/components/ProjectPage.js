d2b48524f6acbf83ea34d64034f40f34ffe1754e
Brett Ranieri <brett.ranieri@gmail.com>
2023-05-19 22:15:38 -0600

    restructure data

--- src/components/ProjectPage.js
+++ src/components/ProjectPage.js
@@ -1,34 +1,12 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
+import { data } from "../projectData";
 
 const ProjectPage = () => {
-	const projects = [
-		{
-			name: "First Project",
-			languages: "HTML, CSS",
-			number: 1,
-		},
-		{
-			name: "Second Project",
-			languages: "JavaScript",
-			number: 2,
-		},
-		{
-			name: "Third Project",
-			languages: "React",
-			number: 3,
-		},
-		{
-			name: "Third Project",
-			languages: "React",
-			number: 4,
-		},
-	];
-
 	return (
 		<div className='project'>
 			<h1>Howdy, I'm the Project Page</h1>
-			{projects.map((project) => (
+			{data.map((project) => (
 				<ProjectCard
 					project={project}
 					key={project.number}
---
+++ src/projectData.js
@@ -0,0 +1,22 @@
+export const data = [
+	{
+		name: "First Fun Project",
+		languages: "HTML, CSS",
+		number: 1,
+	},
+	{
+		name: "Second Project",
+		languages: "JavaScript",
+		number: 2,
+	},
+	{
+		name: "Third Project",
+		languages: "React",
+		number: 3,
+	},
+	{
+		name: "Third Project",
+		languages: "React",
+		number: 4,
+	},
+];
