8c10d3eb04b98dc6530e7277dacaaf68116ae6ea
Brett Ranieri <brett.ranieri@gmail.com>
2023-05-19 22:50:57 -0600

    add scroll icons and make functional

--- src/components/ProjectPage.js
+++ src/components/ProjectPage.js
@@ -1,12 +1,28 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
 import { data } from "../projectData";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 const ProjectPage = () => {
+	const slider = document.getElementById("slider");
+
+	const slideLeft = () => {
+		slider.scrollLeft = slider.scrollLeft - 700;
+	};
+
+	const slideRight = () => {
+		slider.scrollLeft = slider.scrollLeft + 700;
+	};
+
 	return (
 		<div>
 			<h1>Howdy, I'm the Project Page</h1>
 			<div className='relative flex items-center'>
+				<MdChevronLeft
+					className='opacity-50 cursor-point hover:opacity-100'
+					onClick={slideLeft}
+					size={40}
+				/>
 				<div
 					id='slider'
 					className='w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll smooth'
@@ -18,6 +34,11 @@ const ProjectPage = () => {
 						/>
 					))}
 				</div>
+				<MdChevronRight
+					className='opacity-50 cursor-point hover:opacity-100'
+					onClick={slideRight}
+					size={40}
+				/>
 			</div>
 		</div>
 	);
