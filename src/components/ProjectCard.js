5aa1f5afb08470b03cb7a7d2a43adbc003271291
Brett Ranieri <brett.ranieri@gmail.com>
2023-05-19 22:37:21 -0600

    basic styling to create horizontal scroll functionality

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,14 +2,15 @@ import React from "react";
 
 const ProjectCard = ({ project }) => {
 	return (
-		<div className='card'>
+		<div className='w-[400px] h-[400px] inline-block p-2'>
 			<h1>{project.name}</h1>
 			<p>{project.languages}</p>
 			<img
+				className='cursor-pointer hover:scale-105 ease-in-out duration-300'
 				src='https://placehold.co/300x150'
 				alt='placeholder'
 			/>
-			<p>I'm a card for a project and I am here.</p>
+			<p className='whitespace-normal'>{project.description}</p>
 		</div>
 	);
 };
--- src/components/ProjectPage.js
+++ src/components/ProjectPage.js
@@ -4,14 +4,21 @@ import { data } from "../projectData";
 
 const ProjectPage = () => {
 	return (
-		<div className='project'>
+		<div>
 			<h1>Howdy, I'm the Project Page</h1>
-			{data.map((project) => (
-				<ProjectCard
-					project={project}
-					key={project.number}
-				/>
-			))}
+			<div className='relative flex items-center'>
+				<div
+					id='slider'
+					className='w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll smooth'
+				>
+					{data.map((project) => (
+						<ProjectCard
+							project={project}
+							key={project.number}
+						/>
+					))}
+				</div>
+			</div>
 		</div>
 	);
 };
