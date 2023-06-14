6362c90e443dffac78193802ee2a9ab2f3265100
Brett Ranieri <brett.ranieri@gmail.com>
2023-06-14 14:15:19 -0600

    functional code for random picture on click
    multiple attempts with multiple techniques. Ultimately decided to move on and have array empty itself then force a refresh.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,52 +3,143 @@ import { Navbar, Collapse, Typography, Button, IconButton } from "@material-tail
 
 export default function NavBar({ scrollToSection, projects, about, contact }) {
 	const [openNav, setOpenNav] = useState(false);
-	const [previousChoosen, setPreviousChoosen] = useState("");
-	const [needChoosen, setNeedChoosen] = useState(false);
 
 	useEffect(() => {
 		window.addEventListener("resize", () => window.innerWidth >= 960 && setOpenNav(false));
 	}, []);
 
-	useEffect(() => {
-		console.log("choice made");
-		returnPicture(previousChoosen);
-	}, [previousChoosen]);
+	/////////////////////// Brett Method - Random Picture ///////////////////////
+	// const [previousChoosen, setPreviousChoosen] = useState("");
+	// const [needChoosen, setNeedChoosen] = useState(false);
 
-	useEffect(() => {
-		if (needChoosen) {
-			console.log("still needed");
-			console.log(previousChoosen);
-			getPicture();
-			setNeedChoosen(false);
-		} else {
-		}
-	}, [needChoosen]);
+	// useEffect(() => {
+	// 	console.log("choice made");
+	// 	returnPicture(previousChoosen);
+	// }, [previousChoosen]);
 
-	const naturePictures = [254, 443, 567, 889];
+	// useEffect(() => {
+	// 	if (needChoosen) {
+	// 		console.log("still needed");
+	// 		console.log(previousChoosen);
+	// 		getPicture();
+	// 		setNeedChoosen(false);
+	// 	} else {
+	// 	}
+	// }, [needChoosen]);
 
-	function randomPicture() {
-		console.log("rando");
-		const pictures = naturePictures;
-		let previous = previousChoosen;
-		let choosen = pictures[Math.floor(Math.random() * pictures.length)];
-		if (choosen === previous) {
-			setNeedChoosen(true);
-		} else {
-			setNeedChoosen(false);
-			setPreviousChoosen(choosen);
-		}
-	}
+	// const naturePictures = [254, 443, 567];
+
+	// function randomPicture() {
+	// 	console.log("rando");
+	// 	const pictures = naturePictures;
+	// 	let previous = previousChoosen;
+	// 	let choosen = pictures[Math.floor(Math.random() * pictures.length)];
+	// 	console.log("c ", choosen);
+	// 	if (choosen === previous) {
+	// 		setNeedChoosen(true);
+	// 	} else {
+	// 		setNeedChoosen(false);
+	// 		setPreviousChoosen(choosen);
+	// 	}
+	// }
+
+	// function getPicture() {
+	// 	console.log("get");
+	// 	randomPicture();
+	// }
+
+	// function returnPicture(picture) {
+	// 	console.log("im returned ", picture);
+	// 	return picture;
+	// }
+	/////////////////////////////////////////////////////////////////////////
+
+	//////////////////////// Allison Method - Random Picture ////////////////////////////
+
+	// const [useOne, setUseOne] = useState(true);
+
+	// let arrayOne = [
+	// 	{ id: 1, value: 1 },
+	// 	{ id: 2, value: 2 },
+	// 	{ id: 3, value: 3 },
+	// 	{ id: 4, value: 4 },
+	// 	{ id: 5, value: 5 },
+	// ];
+	// let arrayTwo = [];
+
+	// function getPicture() {
+	// 	if (useOne && arrayOne.length > 0) {
+	// 		console.log("One ", arrayOne);
+	// 		let choosen = arrayOne.splice(Math.floor(Math.random() * arrayOne.length), 1);
+	// 		console.log(choosen);
+	// 		arrayTwo.push({ id: choosen[0].id, value: choosen[0].value });
+	// 		console.log("Two ", arrayTwo);
+	// 	} else if (useOne && arrayOne.length === 0) {
+	// 		console.log("ELSE IF!");
+	// 		console.log("Two ", arrayTwo);
+	// 		let choosen = arrayTwo.splice(Math.floor(Math.random() * arrayTwo.length), 1);
+	// 		console.log(choosen);
+	// 		arrayOne.push({ id: choosen[0].id, value: choosen[0].value });
+	// 		console.log("One ", arrayOne);
+	// 	} else if (!useOne) {
+	// 		console.log("don't use one");
+	// 		console.log("Two ", arrayTwo);
+	// 		let choosen = arrayTwo.splice(Math.floor(Math.random() * arrayTwo.length), 1);
+	// 		console.log(choosen);
+	// 		arrayOne.push({ id: choosen[0].id, value: choosen[0].value });
+	// 		console.log("One ", arrayOne);
+	// 	}
+	// }
+
+	// function switchArray() {
+	// 	if (useOne) {
+	// 		setUseOne(false);
+	// 	} else {
+	// 		setUseOne(true);
+	// 	}
+	// }
+
+	// function checkState() {
+	// 	console.log("checking state ", useOne);
+	// }
+
+	// function checkTwo() {
+	// 	console.log("checking Two ", arrayTwo);
+	// }
+
+	// function checkOne() {
+	// 	console.log("checking One ", arrayOne);
+	// }
+
+	///////////////////////////////////////////////////////////////////////////////////
+
+	////////////////////////////// BRETT Second Method ///////////////////////////////////
+
+	const naturePictures = [
+		{ id: 1, value: 1 },
+		{ id: 2, value: 2 },
+		{ id: 3, value: 3 },
+		{ id: 4, value: 4 },
+		{ id: 5, value: 5 },
+	];
+
+	let arrayOfSlices = [];
 
 	function getPicture() {
-		console.log("get");
-		randomPicture();
+		if (naturePictures.length > 0) {
+			console.log("np ", naturePictures.length);
+			let choosen = naturePictures.splice(Math.floor(Math.random() * naturePictures.length), 1);
+			console.log(choosen[0].value);
+			arrayOfSlices.push({ id: choosen[0].id, value: choosen[0].value });
+			console.log("Two ", arrayOfSlices.length);
+		} else if (naturePictures.length === 0) {
+			console.log("you saw them all");
+			window.location.reload();
+		}
 	}
 
-	function returnPicture(picture) {
-		console.log("im returned ", picture);
-		return picture;
-	}
+	///////////////////////////////////////////////////////////////////////////////////////
+
 	const navList = (
 		<ul className='mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6'>
 			<Typography
@@ -124,6 +215,30 @@ export default function NavBar({ scrollToSection, projects, about, contact }) {
 				>
 					<span>Give me some nature!</span>
 				</Button>
+				{/* <Button
+					variant='gradient'
+					size='sm'
+					className='hidden lg:inline-block'
+					onClick={() => checkOne()}
+				>
+					<span>Check One</span>
+				</Button> */}
+				{/* <Button
+					variant='gradient'
+					size='sm'
+					className='hidden lg:inline-block'
+					onClick={() => checkTwo()}
+				>
+					<span>Check Two</span>
+				</Button>
+				<Button
+					variant='gradient'
+					size='sm'
+					className='hidden lg:inline-block'
+					onClick={() => checkState()}
+				>
+					<span>Check State</span>
+				</Button> */}
 				<IconButton
 					variant='text'
 					className='ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden'
