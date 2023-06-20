eb9550b212e55e6a04e963f9999c8f46893bffea
Brett Ranieri <brett.ranieri@gmail.com>
2023-06-19 22:44:40 -0600

    add prelim click event to img

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 const ProjectCard = ({ project }) => {
+	const clickEvent = () => {
+		console.log("clicked");
+	};
+
 	return (
 		<div className='w-[350px] h-[400px] m-2 p-2 proj-card'>
 			<h1 className='proj-title'>{project.name}</h1>
@@ -9,6 +13,7 @@ const ProjectCard = ({ project }) => {
 				className='w-[300px] cursor-pointer hover:scale-75 ease-in-out duration-300'
 				src={project.images[0].source}
 				alt={project.images[0].alt}
+				onClick={() => clickEvent()}
 			/>
 			<p className='whitespace-normal'>{project.shortDescription}</p>
 		</div>
