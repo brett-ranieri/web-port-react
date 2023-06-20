67267271d2894680632f52fe00e20de4b3474b45
Brett Ranieri <brett.ranieri@gmail.com>
2023-06-20 13:37:25 -0600

    basic project page card styling

--- src/components/ProjectPageCard.js
+++ src/components/ProjectPageCard.js
@@ -1,16 +1,38 @@
 import React from "react";
+import { FaGithubSquare } from "react-icons/fa";
 
 const ProjectPageCard = ({ project }) => {
+	// if (project.website === "") return <div>I'm a project that doesn't have a website!</div>;
+
 	return (
-		<div className='proj-page-card inline-block p-2'>
-			<h1>{project.name}</h1>
-			<p>{project.languages}</p>
-			<img
-				className='cursor-pointer hover:scale-105 ease-in-out duration-300'
-				src='https://placehold.co/300x150'
-				alt='placeholder'
-			/>
+		<div className='proj-page-card inline-block p-2 m-4'>
+			<h1 className='proj-title'>{project.name}</h1>
+			<p className='italic'>Languages: {project.languages}</p>
+			<p className='italic'>Technologies: {project.technologies}</p>
+			<div className='img-container flex flex-row justify-center p-2'>
+				{project.images.map((image) => (
+					<img
+						className='h-[250px] hover:scale-105 ease-in-out duration-300 m-2'
+						src={image.source}
+						alt={image.alt}
+					/>
+				))}
+			</div>
 			<p className='whitespace-normal'>{project.description}</p>
+			<div className='link-container flex flex-row justify-center items-center p-2'>
+				<a
+					href={project.website}
+					className='m-2'
+				>
+					Live Website
+				</a>
+				<a
+					href={project.gitLink}
+					className='m-2'
+				>
+					<FaGithubSquare className='btn-icon' />
+				</a>
+			</div>
 		</div>
 	);
 };
