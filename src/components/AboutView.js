2fcd81e071e6377673550898ca6bcb22248c102d
Brett Ranieri <brett.ranieri@gmail.com>
2023-07-06 09:38:29 -0600

    restyle about view

--- src/components/AboutView.js
+++ src/components/AboutView.js
@@ -25,18 +25,22 @@ const AboutView = () => {
 					className={`wedding-img h-[150px] m-3 ${scrollZoomOne ? "h-[700px]" : ""}`}
 					id='wedding_photo'
 				/>
-				<h1 className='marker 	about-view-header text-4xl font-extrabold text-gray-900'>About Me</h1>
-				<div className='about-view-text-container rounded'></div>
-				<p className='about-view-text text-m font-semibold'>
-					After spending most of my life in the Northeast I moved to Colorado following the call of
-					the mountains. Here I met my wife, adopted a small managerie of animals, and decided I
-					wanted a career change. Over a decade in the Entertainment and Event industries provided
-					me with invaluable experience and knowledge that allows me to bring a different
-					perspective to Web Development.
-				</p>
-				<div className='about-btn-style'>
+				<h1 className='marker section-title	about-view-header text-4xl font-extrabold text-gray-900'>
+					About Me
+				</h1>
+				<div className='about-view-text-container rounded pl-2 pt-1'>
+					<p className='about-view-text text-m font-semibold'>
+						After spending most of my life in the Northeast I moved to Colorado following the call
+						of the mountains. Here I met my wife, adopted a small managerie of animals, and decided
+						I wanted a career change. Over a decade in the Entertainment and Event industries
+						provided me with invaluable experience and knowledge that allows me to bring a different
+						perspective to Web Development.
+					</p>
+				</div>
+
+				<div className='about-btn-loc'>
 					<Link
-						className='link-btn hover:scale-105'
+						className='primary-btn py-2 px-4 hover:scale-105'
 						to='/about'
 					>
 						Learn more
