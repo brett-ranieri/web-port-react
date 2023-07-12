9c42afb79dc4228d53030dac6f433c48d533ea43
Brett Ranieri <brett.ranieri@gmail.com>
2023-07-12 13:25:34 -0600

    change back button in picture gallery and project page

--- src/components/PictureGallery.js
+++ src/components/PictureGallery.js
@@ -20,23 +20,21 @@ const PictureGallery = () => {
 
 	return (
 		<div>
-			<div className='flex-container proj-btn-container'>
-				<Link
-					className='primary-btn px-3 py-2'
-					to='/'
-				>
-					Back to Main
-				</Link>
-				{/* <Link
+			<Link
+				className='primary-btn btm-btn px-3 py-2'
+				to='/'
+			>
+				Back to Main
+			</Link>
+			{/* <Link
 					className='link-btn ml-2'
 					to='/about'
 				>
 					Back to About
 				</Link> */}
-			</div>
 
 			<div className='gallery-container h-[100vh]'>
-				<h1 className='light-section-title marker pl-6 pt-3'>Picture Gallery</h1>
+				<h1 className='light-section-title marker pl-6'>Picture Gallery</h1>
 				<div className='h-[58%] flex flex-col justify-center items-center'>
 					<img
 						className='shown-picture'
--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -16,19 +16,17 @@ const ProjectsPage = () => {
 	// }
 
 	return (
-		<div>
-			<div className='flex-container proj-btn-container'>
-				<Link
-					className='secondary-btn py-2 px-4'
-					to='/'
-					onClick={console.log("clicked")}
-				>
-					Back to Main
-				</Link>
-			</div>
+		<div className='proj-container'>
+			<Link
+				className='secondary-btn btm-btn py-2 px-4'
+				to='/'
+				onClick={console.log("clicked")}
+			>
+				Back to Main
+			</Link>
 
-			<div className='proj-container'>
-				<h1 className='section-title marker light-shadow mt-3 pl-6'>Full List of Projects</h1>
+			<div>
+				<h1 className='section-title marker light-shadow pl-6'>Full List of Projects</h1>
 				<p className='section-text pl-10 pb-6 light'>
 					Scroll down to browse all of my projects. The cards on this page contain more details as
 					well as links to view the GitHub Repo and Live Website (if applicable) of each project.
