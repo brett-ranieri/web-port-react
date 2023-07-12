c16af06f0e7a711c9527cece22cc708abed4f6ba
Brett Ranieri <brett.ranieri@gmail.com>
2023-07-12 12:17:09 -0600

    add sections to Project View

--- src/components/ProjectView.js
+++ src/components/ProjectView.js
@@ -6,7 +6,7 @@ const ProjectView = ({ onOpen, modControl }) => {
 	return (
 		<div className='project main-view-comp'>
 			<h1 className='marker ml-1 section-title pt-6 px-4 font-bold'>Highlighted Work</h1>
-			<p className='p-3 ml-6 section-text'>
+			<p className='p-3 ml-6 section-text dark'>
 				The slider below displays some of my favorite recent projects. Click on the screenshots of
 				any project to learn more.
 			</p>
@@ -18,11 +18,37 @@ const ProjectView = ({ onOpen, modControl }) => {
 			/>
 			<div className='flex-container btn-style'>
 				<Link
-					className='secondary-btn py-3 px-6 mt-2'
+					className='secondary-btn py-3 px-6 mt-3 mb-5'
 					to='/projects'
 				>
 					See all Projects
 				</Link>
+			</div>
+			<div className='b-green'>
+				<h1 className='marker ml-1 section-subtitle pt-3 px-4 font-bold light'>Current Project:</h1>
+				<p className='p-4 pb-6 ml-6 section-text light'>
+					I am learning Python as I work through the final module of my bootcamp offered by
+					CareerFoundry
+				</p>
+			</div>
+			<div className='b-light'>
+				<h1 className='marker ml-1 section-subtitle pt-3 px-4 font-bold dark'>
+					Familiar Languages/Technologies:
+				</h1>
+				<ul className='p-3 ml-6 section-text flex flex-row flex-wrap dark'>
+					<li className='p-3'>Node</li>
+					<li className='p-3'>JavaScript</li>
+					<li className='p-3'>HTML</li>
+					<li className='p-3'>CSS</li>
+					<li className='p-3'>MongoDB</li>
+					<li className='p-3'>SQL</li>
+					<li className='p-3'>Express</li>
+					<li className='p-3'>React</li>
+					<li className='p-3'>React Bootstrap</li>
+					<li className='p-3'>SASS</li>
+					<li className='p-3'>Angular</li>
+					<li className='p-3'>Python</li>
+				</ul>
 			</div>
 		</div>
 	);
