77819949cd2820c0ff575627365422187f670032
Brett Ranieri <brett.ranieri@gmail.com>
2023-07-20 17:16:12 -0600

    add some custom colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				dgreen: "#2f341a",
+				lgreen: "#5f6935",
+				tan: "#fece7a",
+			},
+		},
 	},
 	plugins: [require("tailwind-scrollbar-hide")],
 });
